09e10e0a074a80893a41646cc046d26239af08e8
Gui_SamuelBS <153688693+Gu1zinh0@users.noreply.github.com>
2024-07-26 14:14:42 -0300

    Delete Projetos Alura-JS directory

--- Projetos Alura-JS/Desafios-JS/app.js
+++
@@ -1,9 +0,0 @@
-const cliente = {
-    nome: 'Mariana',
-    idade: '35',
-    genero: 'Feminino'
-}
-
-cliente.estado = 'Pernambuco'
-
-alert('Olá, meu nome é ' + cliente.nome + ' e moro no estado de ' + cliente.estado)
